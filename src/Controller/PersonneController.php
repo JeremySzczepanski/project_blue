25ec57dc046422100a388b4ae1225d0006d6710e
JeremySzczepanski <jeremy.szczepanski@gmail.com>
2022-11-25 14:15:10 +0100

    doctrine entityManager Persist et Flush

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PersonneController extends AbstractController
 {
-    #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    #[Route('/personne/add', name: 'personne.add')]
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
+        $entityManager = $doctrine->getManager();
+
+        $personne = new Personne();
+        $personne->setFirstname('Jeremy');
+        $personne->setName('Szczepanski');
+        $personne->setAge('42');
+
+        //Ajouter l'opération d'insertion de la personne dans ma transaction
+        $entityManager->persist($personne);
+
+        //Execute la transaction Todo (methode flash)
+        return $this->render('personne/detail.html.twig', [
+            'personne' => $personne
         ]);
     }
 }
