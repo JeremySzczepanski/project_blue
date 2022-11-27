5b42d15a62e892b947380f17a998ac2893625463
JeremySzczepanski <jeremy.szczepanski@gmail.com>
2022-11-27 11:43:28 +0100

    doctrine Repository findAll

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -8,9 +8,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/personne/add', name: 'personne.add')]
+    #[Route('/', name: 'personne.list')]
+    public function index(ManagerRegistry $doctrine):Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $personnes = $repository->findAll();
+        return $this->render('personne/index.html.twig', [
+            'personnes'=>$personnes
+        ]);
+    }
+
+
+
+    #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
@@ -24,6 +38,9 @@ class PersonneController extends AbstractController
         $entityManager->persist($personne);
 
         //Execute la transaction Todo (methode flash)
+        $entityManager->flush();
+
+
         return $this->render('personne/detail.html.twig', [
             'personne' => $personne
         ]);
---
+++ src/DataFixtures/PersonneFixture.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Personne;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+
+class PersonneFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 100; $i++)
+        {
+            $personne = new Personne();
+            $personne->setFirstname($faker->firstname);
+            $personne->setName($faker->lastname);
+            $personne->setAge($faker->numberBetween(18, 65));
+
+            $manager->persist($personne);
+        }
+
+        $manager->flush();
+    }
+}
